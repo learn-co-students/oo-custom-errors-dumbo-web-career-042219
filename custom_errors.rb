526b28d9df13c9fc915229bf4bf32b89caa2557d
Wei <theweifeng@gmail.com>
2019-05-05 18:02:50 -0400

    Done.

--- custom_errors.rb
+++ custom_errors.rb
@@ -7,11 +7,20 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != person
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
 
 end
 
+class PartnerError < StandardError
+  def message
+    "you must give the get_married method an arguement of an instance of the last person class!"
+  end
+end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
